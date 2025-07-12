e49ff6cd71b7244c99a83619d74a76fd27f2d36d
Levi-Zap <primelevi001@gmail.com>
2025-07-12 10:43:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -38,7 +38,7 @@ CURRENT_STATUS: process.env.CURRENT_STATUS || "true",
 AUTO_REACT: process.env.AUTO_REACT || "false",
 HEART_REACT: process.env.HEART_REACT || "false",
 OWNER_REACT: process.env.OWNER_REACT || "false",
-BOT_NAME: process.env.BOT_NAME || "『 AIZEN 』",
+BOT_NAME: process.env.BOT_NAME || "Aɪᴢᴇɴ",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
 
